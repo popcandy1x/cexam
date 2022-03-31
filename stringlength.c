706a5f45affdd10d48992b027a729fceec35fba3
Venkatesh Jaiswal <popcandy1x@gmail.com>
2022-03-31 17:41:18 +0530

    Added stringlength.c

---
+++ stringlength.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	char ch[100];
+	printf("Enter the String:");
+	gets(ch);
+	int i=0;
+	for(i=0;ch[i]!='\0';i++)
+	{
+
+	}
+	printf("The length of the string is:%d",i);
+	return 0;
+}	
