359c9338585bbce1f3480dbb56c22a07d2a43f7e
Venkatesh Jaiswal <popcandy1x@gmail.com>
2022-03-31 22:02:41 +0530

    Added slowsort.c

---
+++ slowsort.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	int n=0;
+	printf("Enter the length of the array that you want to enter:");
+	scanf("%d",&n);
+	int *ptr;
+	ptr=(int *)calloc(n,sizeof(int));
+	int t=0;
+	if(ptr==NULL)
+	{
+		printf("Space Insufficient to allocate memory\n");
+	}
+	else
+	{
+		printf("Enter the array elements:");
+		for(int i=0;i<n;i++)
+		{
+			scanf("%d",ptr+i);
+		}
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				if(ptr[i]>ptr[j])
+				{
+					t=ptr[i];
+					ptr[i]=ptr[j];
+					ptr[j]=t;
+				}
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			printf("%d ",ptr[i]);
+		}
+	}
+	return 0;
+}
